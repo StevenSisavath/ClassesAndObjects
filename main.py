0b03f55af140d5f3e101f6a8ea3a46e113a9c08a
Steven Sisavath <stevenlsisavath@gmail.com>
2022-10-28 14:47:16 -0500

    Added questions for code review to main.py file

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ from product import Product
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Customer Shopping Cart Tasks
-# 1.
+# 1. Print the customer’s name to the terminal 
 random_customer = Customer(input("What is your name?"))
 print(random_customer.name)
 
-#2. 
+#2. Call the customer’s add product to shopping cart method three times and add the three products objects you created 
 product1 = Product('Milk', 3.98, 'dairy')
 product2 = Product('Nilla Wafers', 4.18, 'snack')
 product3 = Product('Straws', 1.46, 'utensils')
@@ -25,16 +25,16 @@ random_customer.add_new_product(product1.name)
 random_customer.add_new_product(product2.name)
 random_customer.add_new_product(product3.name)
 
-# 3.
+# 3. Call the customer’s view products method 
 random_customer.view_shopping_cart_items()
 
-# 4.
+# 4. Call the customer’s shopping cart’s get cart total method. Capture the total the method returns in a variable and print to the terminal 
 print(random_customer.shopping_cart.return_total_of_list())
 
-# 5.
+# 5. Call the customer’s shopping cart’s empty cart method 
 random_customer.shopping_cart.remove_all_products_in_list()
 
-# 6.
+# 6. Check if all products have been removed from the shopping cart 
 print('')
 print(random_customer.view_shopping_cart_items())
 print('')
