2fb3da06532f131ae951329ca7b8d15251a8834b
Steven Sisavath <stevenlsisavath@gmail.com>
2022-10-28 12:23:31 -0500

    updated alarm_clock.py file to include class (AlarmClock) and updated main.py file to test alarm_clock module.

---
+++ alarm_clock.py
@@ -0,0 +1,29 @@
+class AlarmClock :
+
+    def __init__(self) :
+        self.current_time = 0
+        self.alarm_is_on = True
+        self.alarm_time = 0
+        self.toggle_result = ''
+    
+    def set_current_time(self):
+        self.current_time = input('What is the current time?')
+        print(self.current_time)
+
+    def set_alarm_on_or_off(self):
+        self.toggle = input('Toggle alarm on or off?')
+        if self.toggle == 'on':
+            self.alarm_is_on = True
+            self.toggle_result = 'Alarm is ON'
+        elif self.toggle == 'off':
+            self.alarm_is_on = False
+            self.toggle_result = 'Alarm is OFF'
+        else:
+            self.alarm_is_on = False
+            self.toggle_result = 'Alarm is OFF'
+
+    def set_alarm_time(self):
+        self.alarm_time = input('What time would you like to set your alarm to?')
+        print(self.alarm_time)
+        # return self.alarm_time
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from alarm_clock import AlarmClock
+
+test_alarm_clock = AlarmClock()
+test_alarm_clock.set_current_time()
+test_alarm_clock.set_alarm_time()
+test_alarm_clock.set_alarm_on_or_off()
+print(test_alarm_clock.toggle_result) #test
