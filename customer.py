89c949113f4412690e6db6d856680e76f207e9f0
Steven Sisavath <stevenlsisavath@gmail.com>
2022-10-28 13:30:04 -0500

    Updated 3 files; customer.py, shopping_cart.py, and product.py. Each file has 1 new class added to them for later importing into the main.py file

---
+++ customer.py
@@ -0,0 +1,13 @@
+from shopping_cart import ShoppingCart
+class Customer:
+    
+    def __init__(self, name) :
+        self.name = name
+        self.shopping_cart = ShoppingCart()
+
+    def add_new_product(self, product_to_add):
+        self.shopping_cart = ShoppingCart.add_product_to_list(product_to_add)
+    
+    def view_shopping_cart_items(self):
+        for item in ShoppingCart.list_of_products():
+            print(item)
---
+++ shopping_cart.py
@@ -0,0 +1,12 @@
+class ShoppingCart:
+    def __init__(self):
+        self.list_of_products = []
+
+    def add_product_to_list(self, product_to_add):
+        self.list_of_products += self.list_of_products.append(product_to_add)
+
+    def return_total_of_list(self):
+        return len(self.list_of_products)
+
+    def remove_all_products_in_list(self):
+        self.list_of_products = self.list_of_products.clear()
